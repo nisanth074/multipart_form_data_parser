726baf527d21f8571dd94531cb8b3bebc5a08379
Nisanth Chunduru <nisanth074@gmail.com>
2022-08-31 12:55:28 +0530

    Modify rspec to test untested behaviour

--- spec/multipart_form_data_parser_spec.rb
+++ spec/multipart_form_data_parser_spec.rb
@@ -5,7 +5,8 @@ describe MultipartFormDataParser do
     it "parses multipart form data" do
       multipart_form_data = read_fixture("multipart_form_data1")
       content_type = "multipart/form-data; boundary=xYzZY"
-      MultipartFormDataParser.parse(multipart_form_data, content_type)
+      result = MultipartFormDataParser.parse(multipart_form_data, content_type)
+      expect(result.params["subject"]).to eq("Another test email")
     end
   end
 end
